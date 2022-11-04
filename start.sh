e6c207c421a32cc556aec49df70e3c3d9b22bc30
lazbar1 <81530741+lazbar1@users.noreply.github.com>
2022-11-04 04:34:43 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo, Please Wait..."
-git clone xxxxxx /oracle4
+git clone https://github.com/lazbar1/oracle4.git /oracle4
 cd /oracle4
 echo "Starting Bot, Please Wait..."
 python3 bot.py
